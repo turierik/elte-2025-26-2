0328c0b38d94a33266728b57d136c8b3d1d96758
Tűri Erik <54734700+turierik@users.noreply.github.com>
2026-02-19 15:45:32 +0100

    szerveroldali cs14, 2. gyakorlat

---
+++ Szerveroldali 3-as csoport/blogocska/database/migrations/2026_02_19_134730_create_articles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('content');
+            $table->string('title');
+            $table->foreignId('author_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
--- Szerveroldali 3-as csoport/blogocska/database/seeders/DatabaseSeeder.php
+++ Szerveroldali 3-as csoport/blogocska/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Category;
+use App\Models\Article;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(10) -> create();
+        $categories = Category::factory(5) -> create();
+        $articles = Article::factory(20) -> create();
+        foreach ($categories as $category){
+            $toAttach = $articles -> pluck('id') -> random(rand(1, 5)) -> toArray();
+            $category -> articles() -> attach($toAttach);
+        }
     }
 }
