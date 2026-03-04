6119c8c2bad055e90036056f7f185c5e4f423411
Tűri Erik <54734700+turierik@users.noreply.github.com>
2026-03-04 09:40:39 +0100

    nappali webprog 4. gyakorlat

--- Nappali 3-as csoport/04/index.js
+++ Nappali 3-as csoport/04/index.js
@@ -10,3 +10,76 @@ const visualElements = [
     `<line x1="60%" y1="70%" x2="50%" y2="90%" />`,
     `<line x1="60%" y1="70%" x2="70%" y2="90%" />`
 ];
+
+let guessedLetters;
+let targetWord;
+let gameOver;
+
+const controlsDiv = document.querySelector("#controls");
+const startButton = document.querySelector("#start");
+const resultDiv = document.querySelector("#result");
+const gameDiv = document.querySelector("#game");
+const tr = document.querySelector("tr");
+const buttonsDiv = document.querySelector("#buttons");
+const svg = document.querySelector("svg");
+
+startButton.addEventListener("click", function(){
+    targetWord = wordList[Math.floor(Math.random() * wordList.length)];
+    guessedLetters = [];
+    controlsDiv.style.display = "none";
+    gameDiv.style.display = "block";
+    renderState();
+});
+
+function renderState(){
+    renderWord();
+    renderButtons();
+    renderLittleMan();
+    gameOver = checkEndGame();
+}
+
+function renderWord(){
+    tr.innerHTML = targetWord.split('').map(char => `<td>${
+        guessedLetters.includes(char) ? char : "_"
+    }</td>`).join('');
+}
+
+function renderButtons(){
+    //const alphabet = "ABCEDFGHIJKLMNOPQRTSUVWXYZ".split('');
+    const alphabet = [...Array(26).keys()].map(x => String.fromCharCode(x + 65));
+    buttonsDiv.innerHTML = alphabet.map(letter => `<button id="${letter}" ${
+        guessedLetters.includes(letter.toLowerCase()) ? "disabled" : ""
+    }>${letter}</button>`).join('');
+}
+
+buttonsDiv.addEventListener("click", function(e){
+    if (!gameOver && e.target.matches("button")){
+        guessedLetters.push(e.target.innerText.toLowerCase());
+        renderState();
+    }
+});
+
+document.body.addEventListener("keypress", function(e){
+    const button = document.querySelector(`button#${e.key.toUpperCase()}`);
+    if (button)
+        button.dispatchEvent(new Event("click", { bubbles: true }));
+});
+
+const mistakes = () => guessedLetters.filter(char => !targetWord.includes(char)).length;
+
+function renderLittleMan(){
+    svg.innerHTML = visualElements.slice(0, mistakes()).join('');
+}
+
+function checkEndGame(){
+    if (mistakes() >= visualElements.length ){
+        controlsDiv.style.display = "block";
+        resultDiv.innerText = `You lose. The word was: ${targetWord}`;
+        return true;
+    } else if (targetWord.split('').every(char => guessedLetters.includes(char))){
+        controlsDiv.style.display = "block";
+        resultDiv.innerText = `Congratulations!`;
+        return true;
+    }
+    return false;
+}
