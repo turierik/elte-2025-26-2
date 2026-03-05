a2b11e32fda065a0c939f9ee135b57281e37748f
Tűri Erik <54734700+turierik@users.noreply.github.com>
2026-03-05 17:10:56 +0100

    esti webprog 4. gyakorlat

--- Esti 1-es csoport/04/index.js
+++ Esti 1-es csoport/04/index.js
@@ -18,3 +18,73 @@ const gameDiv = document.querySelector("#game");
 const tr = document.querySelector("tr");
 const buttonsDiv = document.querySelector("#buttons");
 const svg = document.querySelector("svg");
+
+let guessedLetters;
+let targetWord;
+let gameOver;
+
+const mistakes = () => guessedLetters.filter(char => !targetWord.includes(char)).length;
+
+function startGame(){
+    targetWord = wordList[Math.floor(Math.random() * wordList.length)];
+    guessedLetters = [];
+    controlsDiv.style.display = "none";
+    gameDiv.style.display = "block";
+    renderState();   
+}
+
+startButton.addEventListener("click", startGame);
+
+function renderState(){
+    renderWord();
+    renderButtons();
+    renderGraphics();
+    gameOver = checkEndGame();
+}
+
+function renderWord(){
+    tr.innerHTML = targetWord.split('').map(char => `<td>${
+        guessedLetters.includes(char) ? char : "_"
+    }</td>`).join('');
+}
+
+function renderButtons(){
+    // const alphabet = "ABCDEFGHIJKLMONPQRSTUVWXYZ".split('');
+    const alphabet = [...Array(26).keys()].map(x => String.fromCharCode(x + 65));
+    buttonsDiv.innerHTML = alphabet.map(letter => `<button id="${letter}" ${
+        guessedLetters.includes(letter.toLowerCase()) ? "disabled" : ""
+    }>${letter}</button>`).join('');
+}
+
+function handleButtonClick(e){
+    if (!gameOver && e.target.matches("button")){
+        guessedLetters.push(e.target.innerText.toLowerCase());
+        renderState();
+    }
+}
+
+buttonsDiv.addEventListener("click", handleButtonClick);
+
+document.body.addEventListener("keypress", function(e){
+    const button = document.querySelector(`button#${e.key.toUpperCase()}`);
+    button.dispatchEvent(new Event("click", { bubbles: true }));
+});
+
+function renderGraphics(){
+    svg.innerHTML = visualElements.slice(0, mistakes()).join('');
+}
+
+function checkEndGame(){
+    if (mistakes() >= visualElements.length){
+        // vesztettünk
+        resultDiv.innerText = `You lose. The word was: ${targetWord}`;
+        controlsDiv.style.display = "block";
+        return true;
+    } else if ( targetWord.split('').every(char => guessedLetters.includes(char)) ){
+        // nyertünk
+        resultDiv.innerText = `Congratulations`;
+        controlsDiv.style.display = "block";
+        return true;
+    }
+    return false;
+}
